8f8ee93feb1b2c20f815e791c911e3be1214bd20
Daniel <Daniel@SALON.home>
2022-12-18 12:11:40 +0100

    personalizado más y añadidos más métodos

--- src/Principal/ManejadorBDenLocalhost.java
+++ src/Principal/ManejadorBDenLocalhost.java
@@ -8,37 +8,37 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Random;
 
-public class ManejadorBBDD {
+public class ManejadorBDenLocalhost {
 
-	final String DB_URL = "jdbc:mysql://localhost/clase";
+	final String DB_URL = "jdbc:mysql://localhost/";
 	final String USER = "root";
 	final String PASS = "";
 
-	Connection con;
+	Connection conexion;
 
-	public void establecerConexion() {
+	public void establecerConexion(String nombreBD) {
 		try {
-			con = DriverManager.getConnection(DB_URL, USER, PASS);
+			conexion = DriverManager.getConnection(DB_URL.concat(nombreBD), USER, PASS);
 		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("No se puede establecer conexión");
+			System.out.println("No se puede establecer conexión con la BD: " + nombreBD);
 		}
-
+		System.out.println("Conexion establecida con la BD: " + nombreBD);
 	}
 
-	public void cerrarConexion() {
+	public void cerrarConexion(String nombreBD) {
 
 		try {
-			con.close();
+			conexion.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		System.out.println("Conexión cerrada con la BD: " + nombreBD);
 	}
 
 	public void getEmpleados() throws SQLException {
 
 		System.out.println("El listado:");
-		Statement stmt = con.createStatement();
+		Statement stmt = conexion.createStatement();
 		ResultSet rs = stmt.executeQuery("SELECT * FROM empleados");
 
 		while (rs.next()) {
@@ -51,8 +51,8 @@ public class ManejadorBBDD {
 
 	public void getEmpleadosEdad(String edad) throws SQLException {
 
-		System.out.println("El listado:");
-		Statement stmt = con.createStatement();
+		System.out.println("El listado de empleados con la edad " + edad + ":");
+		Statement stmt = conexion.createStatement();
 		ResultSet rs = stmt.executeQuery("SELECT * FROM empleados WHERE edad=".concat(edad));
 
 		while (rs.next()) {
@@ -65,19 +65,20 @@ public class ManejadorBBDD {
 
 	public void crearTablaEmpleados() throws SQLException {
 
-		Statement stmt = con.createStatement();
+		Statement stmt = conexion.createStatement();
 
 		String sql = "CREATE TABLE EMPLEADOS " + "(id INTEGER not NULL AUTO_INCREMENT, " + " nombre VARCHAR(255), "
 				+ " apellidos VARCHAR(255), " + " edad INTEGER, " + " PRIMARY KEY ( id ))";
 
 		stmt.executeUpdate(sql);
-		System.out.println("Tabla creada");
+		System.out.println("Tabla empleados creada con las siguientes columnas:");
+		System.out.println("empleados:[id, nombre, apellidos, edad] y clave principal: id");
 
 	}
 
 	public void borrarTablaEmpleados() throws SQLException {
 
-		Statement stmt = con.createStatement();
+		Statement stmt = conexion.createStatement();
 
 		String sql = "DROP TABLE EMPLEADOS";
 
@@ -86,11 +87,11 @@ public class ManejadorBBDD {
 
 	}
 
-	public void insertarDatosEmpleados() throws SQLException {
+	public void insertarDatosEmpleados(int numNuevosEmpleados) throws SQLException {
 
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < numNuevosEmpleados; i++) {
 			String sql = "INSERT INTO EMPLEADOS (nombre, apellidos, edad) VALUES (?,?,?)";
-			PreparedStatement preparedStatement = con.prepareStatement(sql);
+			PreparedStatement preparedStatement = conexion.prepareStatement(sql);
 			preparedStatement.setString(1, cogerNombreAleatorio());
 			preparedStatement.setString(2, cogerApellidosAleatorio());
 			preparedStatement.setInt(3, ((new Random().nextInt(60))));
@@ -98,8 +99,18 @@ public class ManejadorBBDD {
 
 		}
 
-		System.out.println("Datos insertados");
+		System.out.println("100 Datos aleadorios insertados en tabla empleados");
 
+	}
+
+	public int getNumItems(String tabla) throws SQLException {
+		int num = 0;
+		Statement stmt = conexion.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM ".concat(tabla));
+		while (rs.next()) {
+			num++;
+		}
+		return num;
 	}
 
 	private String cogerNombreAleatorio() {
--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,22 +1,27 @@
 package Principal;
 
 import java.sql.SQLException;
+import java.util.Random;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		ManejadorBBDD mysql = new ManejadorBBDD();
+		ManejadorBDenLocalhost mysql = new ManejadorBDenLocalhost();
 
 		try {
-			mysql.establecerConexion();
+			mysql.establecerConexion("clase");
 			// ya no hace falta borrar, crear ni insertar.
 			// mysql.borrarTablaEmpleados();
 			// mysql.crearTablaEmpleados();
-			// mysql.insertarDatosEmpleados();
+			// mysql.insertarDatosEmpleados(100);
 			// mysql.getEmpleados();
-			mysql.getEmpleadosEdad("19");
+			Random r = new Random();
+			mysql.getEmpleadosEdad(String.valueOf(r.nextInt(60)));
+			System.out.println("El numero de empleados ahora es de " + mysql.getNumItems("empleados"));
+			mysql.insertarDatosEmpleados(100);
+			System.out.println("El numero de empleados ahora es de " + mysql.getNumItems("empleados"));
 
-			mysql.cerrarConexion();
+			mysql.cerrarConexion("clase");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
